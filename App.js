9a617a9d623ca5fb6620f02aee9c294083dfc80d
kokuxiong <kokuxiong@gmail.com>
2021-06-26 16:43:47 +0900

    redux toolkit demo

--- App.js
+++ App.js
@@ -2,13 +2,18 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavi from './navigator/AppNavi';
+import store from './store/store';
+import { Provider } from 'react-redux';
 
 export default function App() {
 
   return (
-    <NavigationContainer>
-      <AppNavi />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavi />
+      </NavigationContainer>
+    </Provider>
+    
     
   );
 }
